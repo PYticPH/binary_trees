b6a7623f9ba0b0df702aa8b62a69963e2fd040cb
PYticPH <pyticph@gmail.com>
2023-03-02 10:42:57 +0100

    Solved: task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Function to check if a node is a root
+ *
+ * @node: pointer to tree node
+ *
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+
+	return (0);
+}
